3b345379c618136a2cffae091f0ebf1ce80d34de
Marco Busemann <marco.busemann@live.com>
2018-08-06 21:22:47 +0200

    Minimal example

---
+++ fuse.js
@@ -0,0 +1,38 @@
+const {FuseBox, WebIndexPlugin} = require("fuse-box");
+
+const fuse = FuseBox.init({
+   homeDir: "src",
+   output: "dist/$name.js",
+   plugins: [
+      WebIndexPlugin({
+         template: "src/client/index.html", 
+         target: "public/index.html", 
+         bundles: [
+            "public/bundle"
+         ]
+      })
+   ]
+});
+
+fuse.dev({
+   port: 4445,
+   httpServer: false,
+});
+
+fuse
+   .bundle("server")
+   .watch("server/**")
+   .target("server@esnext")
+   .instructions(" > [server/index.ts]")
+   .completed(proc => {
+      proc.start();
+   });
+
+fuse
+   .bundle("public/bundle")
+   .watch("client/**")
+   .target("browser@es5")
+   .hmr()
+   .instructions(" > client/index.ts");
+
+fuse.run();
---
+++ src/server/index.ts
@@ -0,0 +1,18 @@
+import * as express from 'express';
+import * as morgan from 'morgan';
+import * as path from 'path';
+
+const app = express();
+
+app.use(morgan('tiny'));
+
+const publicPath = path.resolve('public');
+app.use("/public", express.static(publicPath));
+app.get("*", function(req, res) {
+    res.sendFile(path.join(publicPath, "index.html"));
+});
+
+const port = 3000;
+app.listen(port, () => {
+   console.log(`Server listening on port ${port}!`);
+});
